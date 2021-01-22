8751e0cbdd4c1d8a298a0f935a3e60caed6d8aed
Polaf05 <paularpolo1@gmail.com>
2021-01-22 17:32:32 +0800

    Session 9 express

---
+++ client/src/components/product.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Rating from './rating'
+function Product(props){
+
+    const {product} = props;
+
+    return(
+        <div className="card" key={product._id}>
+        <a href={`/product/${product._id}`}>
+            <img className="medium" src={product.image} alt={product.name} />
+        </a>
+
+        <div className="card-body">
+            <a href={`/product/${product._id}`}>
+                <h2>
+                    {product.name}
+                </h2>
+            </a>
+
+           <Rating rating={product.rating} numReview={product.numReview}></Rating> 
+
+            <div className="price">${product.price}</div>
+        </div>
+    </div>
+    );
+}
+
+export default Product;
---
+++ server/data.js
@@ -0,0 +1,62 @@
+const data ={
+    products:[
+        {
+            _id:"1",
+            name:"John Allen",
+            category: "Male",
+            image: '/images/allen.jpg',
+            price: 69,
+            rating: 1.5,
+            numReview: 69,
+            description: "Master Minecrafter",
+            countInStock: 0
+        },
+        {
+            _id:"2",
+            name:"Clea Bernadette",
+            category: "Female",
+            image: '/images/clea.jpg',
+            price: 96,
+            rating: 2.5,
+            numReview: 10,
+            description: "Bingot",
+            countInStock: 10
+        },
+        {
+            _id:"3",
+            name:"Franz Arvae",
+            category: "Male",
+            image: '/images/franz.jpg',
+            price: 669,
+            rating: 1,
+            numReview: 96,
+            description: "Anthony Look a like",
+            countInStock: 10
+        },
+        {
+            _id:"4",
+            name:"Jan Calvin",
+            category: "Male",
+            image: '/images/jc.jpg',
+            price: 150,
+            rating: 3.5,
+            numReview: 59,
+            description: "ML",
+            countInStock: 10
+        },
+        {
+            _id:"5",
+            name:"Kevin Ralph Lauren",
+            category: "Male",
+            image: '/images/kevin.jpg',
+            price: 500,
+            rating: 4,
+            numReview: 12,
+            description: "happy birthday kiben",
+            countInStock: 10
+        },
+        
+    ]
+}
+
+export default data;
