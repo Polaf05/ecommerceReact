d963b6da3581f7bf654dab3d779a82f7d08e6534
Polaf05 <paularpolo1@gmail.com>
2021-01-24 00:59:47 +0800

    Session 11 Done

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ import data from './data.js'
 
 const app = express();
 
+app.get('/api/products/:id', (req,res) =>{
+    const product = data.products.find((x) => x._id === req.params.id);
+
+    if(product) {
+        res.send(product);
+    }else{
+        res.status(404).send({message:'Product is not found'});
+    }
+});
+
 app.get('/api/products', (req, res) =>{
     res.send(data.products)
 });
@@ -14,5 +24,5 @@ app.get('/',(req, res) =>{
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=> {
-    console.log("serve at http://localhost:$(port)")
+    console.log(`serve at http://localhost:${port}`)
 });
