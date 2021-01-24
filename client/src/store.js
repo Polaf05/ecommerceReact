341992614dc796bbe4dd52dc6c172f11601d4e9c
Polaf05 <paularpolo1@gmail.com>
2021-01-25 02:13:42 +0800

    Session 27 Done

--- client/src/store.js
+++ client/src/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import {productDetailsReducer, productListReducer} from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import { userSigninReducer , userRegisterReducer} from './reducers/userReducers';
+import { orderCreateReducer, orderDetailsReducer,} from './reducers/orderReducers';
 
 const initialState = {
     cart: {
@@ -29,6 +30,8 @@ const reducer = combineReducers({
     cart: cartReducer, 
     userSignin: userSigninReducer,
     userRegister: userRegisterReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 
 })
 const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
