3f3a86c5660932cf7647ff807bb8234050d146d4
Polaf05 <paularpolo1@gmail.com>
2021-01-25 04:17:16 +0800

    Session 33 done

--- client/src/App.js
+++ client/src/App.js
@@ -39,27 +39,26 @@ function App() {
                 </div>
 
                 <div>
+     
                     <Link to="/cart">
                         Cart
                         {cartItems.length > 0 && (
                         <span className="badge">{cartItems.length}</span>
                         )}
                     </Link>
-                    
+
                     {userInfo ? (
                         <div className="dropdown">
                         <Link to="#">
                             {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
                         </Link>
                         <ul className="dropdown-content">
-
-                            <li>
-                                <Link to="/profile">User Profile</Link>
-                            </li>
-                            <li>
-                                <Link to="/orderhistory">Order History</Link>
-                            </li>
-           
+                        <li>
+                                    <Link to="/profile">User Profile</Link>
+                                </li>
+                        <li>
+                                    <Link to="/orderhistory">Order History</Link>
+                                </li>
                             <li>
                             <Link to="#signout" onClick={signoutHandler}>
                                 Sign Out
@@ -69,6 +68,27 @@ function App() {
                         </div>
                     ) : (
                         <Link to="/signin">Sign In</Link>
+                    )}
+                    {userInfo && userInfo.isAdmin && (
+                        <div className="dropdown">
+                        <Link to="#admin">
+                            Admin <i className="fa fa-caret-down"></i>
+                        </Link>
+                        <ul className="dropdown-content">
+                            <li>
+                            <Link to="/dashboard">Dashboard</Link>
+                            </li>
+                            <li>
+                            <Link to="/productlist">Products</Link>
+                            </li>
+                            <li>
+                            <Link to="/orderlist">Orders</Link>
+                            </li>
+                            <li>
+                            <Link to="/userlist">Users</Link>
+                            </li>
+                        </ul>
+                        </div>
                     )}
                 </div>
 
